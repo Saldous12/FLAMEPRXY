d88f3a75dacfb06b83188f479ada46cab556b357
Saldous12 <iidklsdsadol775@gmail.com>
2024-11-13 18:54:12 -0600

    Add files via upload

---
+++ static/tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: {
+    relative: true,
+    files: ['./public/index.html', './public/about.html'],
+    // files: ['./construction/index.html']
+  },
+  theme: {
+    extend: {
+      fontFamily: {
+        'Inter': ['Inter', 'sans-serif']
+      },
+      colors: {
+        'primary': '#0d2c4c',
+        'secondary': '#d36135',
+        'jet-black': '#0c0c0c',
+        'ghost-white': '#f4f4f9'
+      }
+    },
+  },
+  plugins: [
+  ],
+}
